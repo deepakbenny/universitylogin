f4477a2dd9d07788990edf3bca0189ac07d94f3f
deepak <deepakbenny@outlook.com>
2014-07-26 22:12:54 +0530

    options page status message correction

--- options.js
+++ options.js
@@ -1,14 +1,14 @@
 var button = document.getElementById('save');
-button.onclick = hello;
+button.onclick = save_contents;
 
-function hello(){
+function save_contents(){
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
   if(!username.trim() || !password.trim() ){
     document.getElementById("status").innerHTML = "Enter All fields";
   }else{
-    document.getElementById("status").innerHTML = "Enter All fields";
+    document.getElementById("status").innerHTML = "Success! Click the extension";
     localStorage.setItem("username",username);
     localStorage.setItem("password",password);   
   }
