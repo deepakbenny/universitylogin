661c1ff8236cf84ef5caa8d23712cb3399da1d8b
deepak <deepakbenny@outlook.com>
2014-07-28 15:04:55 +0530

    Stable v1.1 status message removed html

--- options.js
+++ options.js
@@ -1,14 +1,23 @@
 var button = document.getElementById('save');
 button.onclick = save_contents;
 
+window.onload = function(){ 
+  alert("asdf");
+  var username = localStorage.getItem("username");
+  var pass = localStorage.getItem("password");
+  document.getElementById("username").value = username;
+  document.getElementById("password").value = pass;
+}
+
+
 function save_contents(){
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
   if(!username.trim() || !password.trim() ){
-    document.getElementById("status").innerHTML = "Enter All fields";
+    document.getElementById("redstatus").innerHTML = "Enter All fields";
   }else{
-    document.getElementById("status").innerHTML = "Success! Click the extension";
+    document.getElementById("greenstatus").innerHTML = "Success! Click the extension";
     localStorage.setItem("username",username);
     localStorage.setItem("password",password);   
   }
--- popup.js
+++ popup.js
@@ -1,18 +1,29 @@
-var username = localStorage.getItem("username");
-var password = localStorage.getItem("password");
-var url = "http://192.168.100.100:8090/login.xml";
+chrome.browserAction.onClicked.addListener(userLogin);
 
-var params = "mode=191&";
-if(!username.trim() || !password.trim() ){
-  document.getElementById("status").innerHTML += "Error";
-}
-else{
-  params += "username=" + username + "&password=" + password;
-  params += "&producttype=0";
-  xhttp = new XMLHttpRequest();
-  xhttp.open("POST",url,true);
-  xhttp.send(params);
-
-  //add id status=200 then okay
-  document.getElementById("status").innerHTML = "Connected";
+function userLogin(){ 
+  var username = localStorage.getItem("username");
+  var password = localStorage.getItem("password");
+  var url = "http://192.168.100.100:8090/login.xml";
+  var params = "mode=191&";
+  if(username == null || password == null ){
+    //document.getElementById("redstatus").innerHTML = "Error </br>Fill in options page";
+  }
+  else{
+    params += "username=" + username + "&password=" + password;
+    params += "&producttype=0";
+    xhttp = new XMLHttpRequest();
+    xhttp.open("POST",url,true);
+    xhttp.send(params);
+    xhttp.onreadystatechange = function() {
+      if(xhttp.readyState == 4) {
+        if(xhttp.responseText.indexOf("logged in") != -1)
+          alert("Connected");
+        else
+          alert("Wrong username or password");
+      }
+    }
+    //add id status=200 then okay
+   // document.getElementById("login").innerHTML = "Logged In";
+    //document.getElementById("greenstatus").innerHTML = "Connected";
+  }
 }
